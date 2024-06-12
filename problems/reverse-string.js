a4a148550cd161b09754aa67ce25bdd336aa93cd
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-12 19:05:48 +0100

    testing reverse-string function

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,8 @@
-module.exports = function reverseString(string) {
-  // Your code here
-};
+function reverseString(str) {
+  if (typeof str !== 'string') {
+    throw new TypeError('Input must be a string');
+  }
+  return str.split('').reverse().join('');
+}
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const { expect } = require('chai');
+const reverseString = require('../problems/reverse-string');
+
+describe ('reverseString', () => {
+    it('should reverse the string "fun" to "nuf"', () => {
+        const input = 'fun';
+        const expectedOutput = 'nuf';
+        expect(reverseString(input)).to.equal(expectedOutput);
+    })
+
+    it('should return an empty string if input is an empty string', () => {
+        const input = '';
+        const expectedOutput = '';
+        expect(reverseString(input)).to.equal(expectedOutput);
+    })
+
+    it('should handle a string with one character', () => {
+        const input = 'a';
+        const expectedOutput = 'a';
+        expect(reverseString(input)).to.equal(expectedOutput);
+    })
+
+    it('should handle a string with spaces', () => {
+        const input = 'hello world';
+        const expectedOutput = 'dlrow olleh';
+        expect(reverseString(input)).to.equal(expectedOutput);
+    })
+
+    it('should throw an error if input is not a string', () => {
+        const input = 123;
+        expect(() => reverseString(input)).to.throw(TypeError, 'Input must be a string');
+    })
+})

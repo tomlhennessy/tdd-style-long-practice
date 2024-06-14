4bb9b35973c5f5c6dec9907bee13dbae635bf87d
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-14 17:46:51 +0100

    wrote tests for returnsThree

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,8 @@
-// Your code here
+const { expect } = require('chai');
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe('returnsThree', () => {
+    it('should return the number 3', () => {
+        expect(returnsThree()).to.equal(3);
+    })
+})
